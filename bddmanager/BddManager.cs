b72adb2c348f92925f93a060d328f0220ebe7f5d
Ethan-CNED <ethanboulanger2@gmail.com>
2025-04-27 17:56:03 +0200

    Ajoutez des fichiers projet.

---
+++ bddmanager/BddManager.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kanban.bddmanager
+{
+    internal class BddManager
+    {
+        public static class DatabaseManager
+        {
+            private const string ConnectionString = "votre_chaine_de_connexion";
+
+            public static SqlConnection GetConnection()
+            {
+                return new SqlConnection(ConnectionString);
+            }
+
+            public static void ExecuteQuery(string query)
+            {
+                using (SqlConnection connection = GetConnection())
+                {
+                    connection.Open();
+                    SqlCommand command = new SqlCommand(query, connection);
+                    command.ExecuteNonQuery();
+                }
+            }
+        }
+
+    }
+}
